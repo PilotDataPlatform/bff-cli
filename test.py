5dee2aecd2efe391a3d707f0cb5770503aa72b85
agu <agu@indocresearch.org>
2022-03-22 14:20:20 -0400

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from app.resources.helpers import query_node
-import asyncio
-
-
-paylod =  {'query': {'name': 'fake_file', 'display_path': 'fake_file', 'archived': False, 'project_code': 'cli', 'labels': ['File', 'gr']}}
-result = asyncio.run(query_node(payload=paylod))
-print(result)
